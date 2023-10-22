c18df2581421b6daaad60e32f5c30eb6937424d2
Aayush181509 <aayush.181509@ncit.edu.np>
2023-10-22 13:16:21 +0545

    GeoDjango models, and other files added

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.gis",
+    "world",
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,17 @@ WSGI_APPLICATION = 'geodjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "geodjango",
+        "USER": "geo",
+        "PASSWORD" : "somepassword"
+        
+    },
 }
 
 
